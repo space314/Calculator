e215f02f6df4f8e2d05b31cefed93a0dcf69a0d9
Jesse <jmkeller4@alaska.edu>
2018-02-14 16:13:50 -0900

    tests added

--- main.cpp
+++ main.cpp
@@ -1,10 +1,39 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
+#include <string>
 
 
 
 
-TEST_CASE("NULL STRING TEST")
+
+int calculator(const std::string & s)
 {
+	if (s == "")
+	{
+		return 0;
+	}
 
+	int i = 0;
+	int sum = 0;
+	while (isdigit(s[i]))
+	{
+		sum = 10 * sum + (int)(s[i] - '0');
+		i++;
+	}
+
+	return sum;
+}
+
+
+
+TEST_CASE("NULL STRING TEST", "[NULL]")
+{
+	REQUIRE(calculator("") == 0);
+}
+
+TEST_CASE("Single Digit", "[Single Digit]")
+{
+	REQUIRE(calculator("5") == 5);
+	REQUIRE(calculator("0") == 0);
+	REQUIRE(calculator("9") == 9);
 }
